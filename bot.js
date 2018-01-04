376a416f70360888819a968af2013981335b6e1d
designerSohail <designersohail786@gmail.com>
2018-01-04 23:46:29 +0545

    Follow handling added(When someone follows send message as hello {username}. How are you

--- bot.js
+++ bot.js
@@ -7,12 +7,31 @@ const operation = {
   "doTwit" : msg => {
     twit.post('statuses/update',
       { status: msg },
-      function(err, data, response) {
+      (err, data, response) => {
       if (err) {
         console.log("An Error Occured !" + err.message);
       } else {
         console.log("Successfully tweeted !");
       }
     });
+  },
+  "sendMessage" : (id, name, msg) => {
+    twit.post(
+      "direct_messages/new",
+      {
+        user_id : id,
+        screen_name : name,
+        text : msg
+      },
+      (err, data, res) => {
+        if (err) {
+          this.sendMessage(807594238535868421, "designer_sohail", err.message);
+        }
+      }
+    );
   }
 }
+stream.on("follow", e => {
+  console.dir(e.source.screen_name + " followed !");
+  operation.sendMessage(e.source.id, e.source.screen_name, `Hello ${e.source.screen_name}. How are you!`);
+})
