fac323fddd49a3b6e95f1d9835d6c4c825210ef8
designerSohail <designersohail786@gmail.com>
2018-01-08 22:45:10 +0545

    Bugs Fixed

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 console.log("Bot is running !");
-const Twit = require("./twit");
+const Twit = require("twit");
 const config = require("./config");
 const twit = new Twit(config);
 const stream = twit.stream("user");
